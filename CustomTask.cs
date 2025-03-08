cd08d3e6714e1c4be065f9bd123f76eb632ae5cb
Francisco Arturo Licon Colon <franciscoliconcolon@gmail.com>
2025-03-07 23:47:07 -0600

    Create Task from scratch. Add Run, ContinueWith and Wait

---
+++ CustomTask.cs
@@ -0,0 +1,123 @@
+using System.Runtime.ExceptionServices;
+
+namespace CreatingTaskFromScratch;
+
+public class CustomTask
+{
+    private readonly Lock _lock = new();
+    private bool _completed;
+    private Exception? _exception;
+    private Action? _action;
+    private ExecutionContext? _context;
+
+    public bool IsCompleted
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _completed;
+            }
+        }
+    }
+
+    public static CustomTask Run(Action action)
+    {
+        CustomTask task = new();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            try
+            {
+                action();
+                task.SetResult();
+            }
+            catch (Exception e)
+            {
+                task.SetException(e);
+            }
+        });
+        
+        return task;
+    }
+    
+    public void Wait()
+    {
+        ManualResetEventSlim? resetEventSlim = null;
+
+        lock (_lock)
+        {
+            if (!_completed)
+            {
+                resetEventSlim = new();
+                ContinueWith(() => resetEventSlim.Set());
+            }
+        }
+       
+        resetEventSlim?.Wait();
+        
+        if (_exception is not null)
+        {
+            ExceptionDispatchInfo.Throw(_exception);
+        }
+    }
+
+    public CustomTask ContinueWith(Action action)
+    {
+        CustomTask task = new();
+
+        lock (_lock)
+        {
+            if (_completed)
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    try
+                    {
+                        action();
+                        task.SetResult();
+                    }
+                    catch (Exception e)
+                    {
+                        task.SetException(e);
+                    }
+                });
+            }
+            else
+            {
+                _action = action;
+                _context = ExecutionContext.Capture();
+            }
+        }
+        
+        return task;
+    }
+
+    private void SetResult() => CompleteTask(null);
+    
+    private void SetException(Exception exception) => CompleteTask(exception);
+
+    private void CompleteTask(Exception? exception)
+    {
+        lock (_lock)
+        {
+            if (_completed)
+                throw new InvalidOperationException(
+                    "Task already completed. Cannot set an exception of a completed Task");
+            
+            _completed = true;
+            _exception = exception;
+
+            if (_action is not null)
+            {
+                if (_context is null)
+                {
+                    _action();
+                }
+                else
+                {
+                    ExecutionContext.Run(_context, state => ((Action?)state)?.Invoke(), _action);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CreatingTaskFromScratch;
+
+Console.WriteLine($"Starting Thread id: {Environment.CurrentManagedThreadId}");
+
+CustomTask.Run(() =>
+    {
+        Console.WriteLine($"First CustomTask Thread id: {Environment.CurrentManagedThreadId}");
+    }).Wait();
+
+CustomTask.Run(() =>
+{
+    Console.WriteLine($"Second CustomTask Thread id: {Environment.CurrentManagedThreadId}");
+}).Wait();
